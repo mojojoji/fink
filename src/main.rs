549f08a3c6ba04d9eedc674b2333eb5abb8709c0
Joji Augustine <jojimail@gmail.com>
2024-01-30 22:51:13 +0000

    working controller

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 use axum::{response::Html, routing::get, Json, Router};
 
+use controller::State;
 use serde_json::{json, Value};
 use tokio::join;
 
 #[tokio::main]
 async fn main() {
-    let controller_run = tokio::spawn(async {
-        controller::run().await;
-    });
+    let state = State::default();
+    controller::run(state.clone()).await;
 
-    // build our application with a route
-    let app: Router = Router::new()
-        .route("/metrics", get(metrics))
-        .route("/health", get(health));
+    // // build our application with a route
+    // let app: Router = Router::new()
+    //     .route("/metrics", get(metrics))
+    //     .route("/health", get(health));
 
-    // run it
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
+    // // run it
+    // let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    //     .await
+    //     .unwrap();
+    // println!("listening on {}", listener.local_addr().unwrap());
 
-    axum::serve(listener, app).await.unwrap();
+    // axum::serve(listener, app).await.unwrap();
 
-    join!(controller_run).0.unwrap();
+    // join!(controller_run).0.unwrap();
 }
 
 async fn health() -> Json<Value> {
