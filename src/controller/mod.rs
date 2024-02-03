4eb8974d3d42d4baa9b558c6311c51df57a80dde
Joji Augustine <jojimail@gmail.com>
2024-02-03 19:51:03 +0100

    make it work with pods ans services

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -5,6 +5,7 @@ use crate::{
     utils::Result,
 };
 use futures::StreamExt;
+use k8s_openapi::api::core::v1::{Pod, Service};
 use kube::{
     api::{Api, ListParams, ResourceExt},
     client::Client,
@@ -29,19 +30,19 @@ pub struct Context {
 
 async fn reconcile(vm: Arc<VirtualMachine>, ctx: Arc<Context>) -> Result<Action> {
     let ns = vm.namespace().unwrap(); // doc is namespace scoped
-    let pokemons: Api<VirtualMachine> = Api::namespaced(ctx.client.clone(), &ns);
+    let vms: Api<VirtualMachine> = Api::namespaced(ctx.client.clone(), &ns);
 
     info!("Reconciling \"{}\" in {}", vm.name_any(), ns);
-    finalizer(&pokemons, VIRTUAL_MACHINE_FINALIZER, vm, |event| async {
+    finalizer(&vms, VIRTUAL_MACHINE_FINALIZER, vm, |event| async {
         match event {
-            Finalizer::Apply(pokemon) => pokemon.reconcile(ctx.clone()).await,
-            Finalizer::Cleanup(pokemon) => pokemon.cleanup(ctx.clone()).await,
+            Finalizer::Apply(vm) => vm.reconcile(ctx.clone()).await,
+            Finalizer::Cleanup(vm) => vm.cleanup(ctx.clone()).await,
         }
     })
     .await
     .map_err(|e| Error::FinalizerError(Box::new(e)))
 }
-fn error_policy(_pokemon: Arc<VirtualMachine>, error: &Error, _ctx: Arc<Context>) -> Action {
+fn error_policy(_vm: Arc<VirtualMachine>, error: &Error, _ctx: Arc<Context>) -> Action {
     warn!("reconcile failed: {:?}", error);
     Action::requeue(Duration::from_secs(5 * 60))
 }
@@ -51,14 +52,18 @@ pub async fn run(state: AppState) {
     let client = Client::try_default()
         .await
         .expect("failed to create kube Client");
-    let pokemons = Api::<VirtualMachine>::all(client.clone());
+    let vms = Api::<VirtualMachine>::all(client.clone());
+    let pods = Api::<Pod>::all(client.clone());
+    let services = Api::<Service>::all(client.clone());
 
-    if let Err(e) = pokemons.list(&ListParams::default().limit(1)).await {
+    if let Err(e) = vms.list(&ListParams::default().limit(1)).await {
         error!("CRD is not queryable; {e:?}. Is the CRD installed?");
         info!("Installation: cargo run --bin crdgen | kubectl apply -f -");
         std::process::exit(1);
     }
-    Controller::new(pokemons, Config::default().any_semantic())
+    Controller::new(vms, Config::default().any_semantic())
+        .owns(pods, Config::default().any_semantic())
+        .owns(services, Config::default().any_semantic())
         .shutdown_on_signal()
         .run(reconcile, error_policy, state.to_context(client))
         .filter_map(|x| async move { std::result::Result::ok(x) })
--- src/controller/virtualmachine.rs
+++ src/controller/virtualmachine.rs
@@ -3,12 +3,17 @@
 use crate::{controller::Context, errors::Error, utils::Result};
 use std::{sync::Arc, time::Duration};
 
-use k8s_openapi::api::core::v1::Pod;
+use k8s_openapi::api::core::v1::{
+    Container, Pod, PodSpec, PodStatus, Service, ServicePort, ServiceSpec,
+};
+use k8s_openapi::apimachinery::pkg::util::intstr::IntOrString;
+use kube::error::ErrorResponse;
 use kube::{
     api::{Api, Patch, PatchParams, PostParams, ResourceExt},
     client::Client,
+    core::ObjectMeta,
     runtime::controller::Action,
-    CustomResource,
+    CustomResource, Resource,
 };
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -40,9 +45,14 @@ pub enum VirtualMachineCurrentState {
     group = "codesandbox.io",
     version = "v1alpha1",
     kind = "VirtualMachine",
-    namespaced
+    namespaced,
+    doc = "A VirtualMachine resource for FinK",
+    singular = "virtualmachine",
+    plural = "virtualmachines",
+    shortname = "vm",
+    status = "VirtualMachineStatus",
+    printcolumn = r#"{"name":"Image", "type":"string", "description":"VM rootfs image", "jsonPath":".spec.image"}"#
 )]
-#[kube(status = "VirtualMachineStatus", shortname = "poke")]
 pub struct VirtualMachineSpec {
     pub image: String,
     pub state: VirtualMachineDesiredState,
@@ -56,62 +66,13 @@ pub struct VirtualMachineStatus {
 impl VirtualMachine {
     // Reconcile (for non-finalizer related changes)
     pub async fn reconcile(&self, ctx: Arc<Context>) -> Result<Action> {
-        let client: Client = ctx.client.clone();
-
-        let ns = self.namespace().unwrap();
-        let name = self.name_any();
         // let vms: Api<VirtualMachine> = Api::namespaced(client, &ns);
 
-        info!("Reconciling VirtualMachine {} in {}", name, ns);
-
-        // Create a pod in the ns
-        let pod = serde_json::from_value(serde_json::json!({
-            "apiVersion": "v1",
-            "kind": "Pod",
-            "metadata": {
-                "name": self.metadata.name,
-            },
-            "spec": {
-                "containers": [{
-                    "name": self.metadata.name,
-                    "image": self.spec.image,
-                }],
-            },
-        }))
-        .unwrap();
-
-        let pods: Api<Pod> = Api::namespaced(client, &ns);
-        let _o = pods
-            .create(&PostParams::default(), &pod)
-            .await
-            .map_err(Error::KubeError)?;
-
-        // let should_alive = self.spec.health > 0;
-        // if !self.is_alive() && should_alive {
-        //     println!("Pokemon \"{}\" is alive", name);
-
-        //     // ==============================
-        //     // This is where you would send events to kubernetes
-        //     // ==============================
-        // }
-
-        // ==============================
-        // Do the actual work here to apply changes to the kubernetes
-        // ==============================
-        // always overwrite status object with what we saw
-        // let new_status = Patch::Apply(json!({
-        //     "apiVersion": "pokemon.rs/v1",
-        //     "kind": "Pokemon",
-        //     "status": PokemonStatus {
-        //         alive: should_alive,
-        //     }
-        // }));
-
-        // let ps: PatchParams = PatchParams::apply("cntrlr").force();
-        // let _o = docs
-        //     .patch_status(&name, &ps, &new_status)
-        //     .await
-        //     .map_err(Error::KubeError)?;
+        match self.spec.state {
+            VirtualMachineDesiredState::STOPPED => self.stop(ctx).await?,
+            VirtualMachineDesiredState::STARTED => self.start(ctx).await?,
+            VirtualMachineDesiredState::HIBERNATED => self.hibernate(ctx).await?,
+        }
 
         // If no events were received, check back every 5 minutes
         Ok(Action::requeue(Duration::from_secs(5 * 60)))
@@ -127,5 +88,161 @@ impl VirtualMachine {
         // ==============================
 
         Ok(Action::await_change())
+    }
+
+    async fn update_status(&self, ctx: Arc<Context>, status: VirtualMachineStatus) -> Result<()> {
+        let ns = self.namespace().unwrap();
+        let vm_name = self.metadata.name.as_ref().unwrap();
+
+        let vms: Api<VirtualMachine> = Api::namespaced(ctx.client.clone(), &ns);
+        let patch = Patch::Merge(status);
+        let _o = vms
+            .patch_status(vm_name, &PatchParams::default(), &patch)
+            .await
+            .map_err(Error::KubeError)?;
+        Ok(())
+    }
+
+    async fn start(&self, ctx: Arc<Context>) -> Result<()> {
+        let client: Client = ctx.client.clone();
+        let ns = self.namespace().unwrap();
+
+        let owner_reference = self.controller_owner_ref(&()).unwrap();
+
+        let vm_name = self.metadata.name.as_ref().unwrap();
+        let image = self.spec.image.clone();
+        let vm_label_key = "vms.codesandbox.io/name".to_string();
+
+        let mut labels = self.metadata.labels.clone().unwrap_or_default();
+        labels.insert(vm_label_key, vm_name.to_string());
+
+        // Create a pod in the ns
+        let pod = Pod {
+            metadata: ObjectMeta {
+                name: Some(vm_name.to_string()),
+                owner_references: Some(vec![owner_reference.clone()]),
+                labels: Some(labels.clone()),
+                ..ObjectMeta::default()
+            },
+            spec: Some(PodSpec {
+                containers: vec![Container {
+                    name: "vm-container".to_string(),
+                    image: Some(image),
+                    ..Container::default()
+                }],
+                ..PodSpec::default()
+            }),
+            ..Pod::default()
+        };
+
+        let service = Service {
+            metadata: ObjectMeta {
+                name: Some(vm_name.to_string()),
+                owner_references: Some(vec![owner_reference]),
+                labels: Some(labels.clone()),
+                ..ObjectMeta::default()
+            },
+            spec: Some(k8s_openapi::api::core::v1::ServiceSpec {
+                selector: Some(labels.clone()),
+                ports: Some(vec![k8s_openapi::api::core::v1::ServicePort {
+                    protocol: Some("TCP".to_string()),
+                    port: 80,
+                    target_port: Some(IntOrString::Int(80)),
+                    ..ServicePort::default()
+                }]),
+                ..ServiceSpec::default()
+            }),
+            ..Service::default()
+        };
+
+        let pods: Api<Pod> = Api::namespaced(client.clone(), &ns);
+        let existing_pod = pods.get(vm_name).await;
+        if let Err(kube::Error::Api(ErrorResponse { code, .. })) = existing_pod {
+            if code == 404 {
+                let _o = pods
+                    .create(&PostParams::default(), &pod)
+                    .await
+                    .map_err(Error::KubeError)?;
+            }
+        }
+
+        let services: Api<Service> = Api::namespaced(client, &ns);
+        let existing_service = services.get(vm_name).await;
+        if let Err(kube::Error::Api(ErrorResponse { code, .. })) = existing_service {
+            if code == 404 {
+                let _o = services
+                    .create(&PostParams::default(), &service)
+                    .await
+                    .map_err(Error::KubeError)?;
+            }
+        }
+
+        if let Ok(Pod {
+            status:
+                Some(PodStatus {
+                    container_statuses: Some(container_statuses),
+                    ..
+                }),
+            ..
+        }) = existing_pod
+        {
+            let all_started = container_statuses
+                .into_iter()
+                .all(|cs| cs.started.unwrap_or(false));
+
+            if all_started {
+                self.update_status(
+                    ctx.clone(),
+                    VirtualMachineStatus {
+                        state: VirtualMachineCurrentState::STARTED,
+                    },
+                )
+                .await?;
+            }
+        } else {
+            self.update_status(
+                ctx.clone(),
+                VirtualMachineStatus {
+                    state: VirtualMachineCurrentState::STARTING,
+                },
+            )
+            .await?;
+        }
+
+        Ok(())
+    }
+
+    async fn stop(&self, ctx: Arc<Context>) -> Result<()> {
+        let client: Client = ctx.client.clone();
+
+        let ns = self.namespace().unwrap();
+        let name = self.name_any();
+        info!("Creating VirtualMachine {} in {}", name, ns);
+        let vm_name = self.metadata.name.as_ref().unwrap();
+
+        let pods: Api<Pod> = Api::namespaced(client.clone(), &ns);
+        let existing_pod = pods.get(vm_name).await;
+        if existing_pod.is_ok() {
+            let _o = pods
+                .delete(vm_name, &Default::default())
+                .await
+                .map_err(Error::KubeError)?;
+        }
+
+        let services: Api<Service> = Api::namespaced(client, &ns);
+        let existing_service = services.get(vm_name).await;
+        if existing_service.is_ok() {
+            let _o = services
+                .delete(vm_name, &Default::default())
+                .await
+                .map_err(Error::KubeError)?;
+        }
+
+        info!("Stopping VirtualMachine {}", self.name_any());
+        Ok(())
+    }
+    async fn hibernate(&self, _ctx: Arc<Context>) -> Result<()> {
+        info!("Hibernating VirtualMachine {}", self.name_any());
+        Ok(())
     }
 }
